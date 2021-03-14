1e16a343568cf8115dc2588cd92e2f5edd7998fb
williamwjd <william.jose.wjd@gmail.com>
2021-03-14 11:25:12 -0300

    validaçao de cadastro de usuario

--- src/pages/SignOut/index.tsx
+++ src/pages/SignOut/index.tsx
@@ -1,9 +1,14 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import getValidateErrors from '../../utils/getValidationErrors';
 
 import { Container, Content } from './styles';
 
@@ -12,15 +17,34 @@ interface FormProps{
 }
 
 const SignOut: React.FC = () => {
-  const handleSubmit = useCallback((data:FormProps) => {
-    console.log(data);
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data:FormProps) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string().email('Digite um e-mail válido').required('E-mail é obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      toast.success('Usuário cadastrado com sucesso!');
+    } catch (err) {
+      toast.error('Erro ao cadastrar usuário!');
+      const errors = getValidateErrors(err);
+      formRef.current?.setErrors(errors);
+    }
   }, []);
+
   return (
     <Container>
       <Content>
         <h1>Dados cadastrais</h1>
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <Input
             name="name"
             placeholder="Digite o seu nome"
@@ -35,12 +59,6 @@ const SignOut: React.FC = () => {
             name="password"
             type="password"
             placeholder="Digite sua senha secreta"
-          />
-
-          <Input
-            name="passwordConfirmation"
-            type="password"
-            placeholder="Confirmar senha"
           />
 
           <Button title="Salvar" />
